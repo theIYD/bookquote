68203b8ebc3bf6ca1eb3116cf80f161334a78c86
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2021-01-27 17:29:36 +0530

    fix: letter spacing in header text

--- components/HeaderText/index.js
+++ components/HeaderText/index.js
@@ -3,7 +3,12 @@ import { Flex, Heading } from "@chakra-ui/react";
 export default function HeaderText({ text }) {
   return (
     <Flex justifyContent="center" alignItems="center" my={6}>
-      <Heading letterSpacing="-0.09em" as="h1" size="3xl" fontWeight="800">
+      <Heading
+        letterSpacing={["-0.06em", "-0.06em", "-0.09em"]}
+        as="h1"
+        size="3xl"
+        fontWeight="800"
+      >
         {text}
       </Heading>
     </Flex>
